2be6c2bcd367fd8b00941717c93ac170334917e4
Rhithick02 <rhithick02@gmail.com>
2020-04-27 09:20:24 +0530

    Script 3

--- Task 1/script3.sh
+++ Task 1/script3.sh
@@ -5,7 +5,7 @@ cd
 #Copying program to a another file in home directory
 echo "#!/bin/bash
 date=\$(date +\"%Y-%m-%d\")
-cat position.log | grep date | while read pos
+cat position.log | grep \$date | while read pos
 do
 	echo \$var | cat > temp.txt
 	user=\$(awk '{print \$2}' temp.txt)
